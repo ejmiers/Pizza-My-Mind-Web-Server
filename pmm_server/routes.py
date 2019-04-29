eeb474d49f3035ea27aad3e923ba15fee5792ba3
Eric Miers <ejmiers@msn.com>
2019-04-29 01:35:09 -0400

    Added routes for creating new events and event sessions

--- pmm_server/routes.py
+++ pmm_server/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect
 from pmm_server import app, db, bcrypt
 from pmm_server.db_models import Student, Administrator, Event, Attendance
-from pmm_server.forms import StudentSignInForm, AdminSignInForm
+from pmm_server.forms import StudentSignInForm, AdminSignInForm, NewEventForm, IDReaderForm
 from pmm_server.date_models import Date
 from flask_login import login_user, current_user, logout_user
 from functools import wraps
@@ -69,10 +69,95 @@ def adminLogin():
 @app.route("/admin-console", methods=['GET', 'POST'])
 @login_required
 def adminConsole():
-    # if Student.query.filter_by(id=current_user.get_id).first():
-    #     flash('You do not have permission to access this page.', 'danger')
-    #     return redirect(url_for('home'))
-    return "<p1>Pardon our dust! Console under construction.</p1>"
+    return render_template('admin.html', title='Admin Console')
+
+@app.route("/admin-console/start-event", methods=['GET', 'POST'])
+@login_required
+def adminConsole_startEvent():
+    date = Date()
+    eventChoices = [(str(g.id), g.eventName) for g in Event.query.filter_by(semester=date.season).filter_by(year=date.year).all()]
+    eventChoices.insert(0,('none', 'none (new event session)'))
+
+    form = NewEventForm()
+    form.attendance.choices = eventChoices
+
+    if form.validate_on_submit():
+        if form.attendance.data != 'none':
+            eventAttendance = Event.query.filter_by(id=form.attendance.data)\
+                                         .filter_by(semester=date.season)\
+                                         .filter_by(year=date.year).first()
+            attendanceEntries = Attendance.query.filter_by(eventID=eventAttendance.id).all()
+            event = Event(eventName=form.eventName.data, attendanceTotal=eventAttendance.attendanceTotal,
+                          newStudentTotal=0, semester=date.season, year=date.year)
+            db.session.add(event)
+            db.session.commit()
+
+            for entry in attendanceEntries:
+                newEntry = Attendance(event.id, entry.studentID, 0, entry.semester, entry.year)
+                db.session.add(newEntry)
+
+            db.session.commit()
+        else:
+            event = Event(eventName=form.eventName.data, attendanceTotal=0,
+                          newStudentTotal=0, semester=date.season, year=date.year)
+            db.session.add(event)
+            db.session.commit()
+
+        pageTitle = "event: " + str(event.eventName)
+        return redirect(url_for('adminConsole_eventSession', event_url=event.id))
+
+    return render_template('create_event.html', title='start new event', admin=current_user, form=form)
+
+@app.route("/admin-console/event/eventID='<event_url>'", methods=['GET', 'POST'])
+@login_required
+def adminConsole_eventSession(event_url):
+    date = Date()
+    event = Event.query.filter_by(id=event_url).first()
+
+    if not event:
+        flash(f'Cannot open event session for event id: {event_url}', 'danger')
+        return redirect(url_for('adminConsole_startEvent'))
+
+    form = IDReaderForm()
+    if form.validate_on_submit():
+        id = form.id.data
+        if id.startswith('20'):
+            id = id[2:]
+
+        student = Student.query.filter_by(id=id).first()
+
+        if (not Attendance.query.filter_by(eventID=event.id).filter_by(studentID=id).first()):
+            event.attendanceTotal += 1
+
+            if student == None:
+                isNew = True
+            else:
+                isNew = False
+
+            if isNew:
+                newStudent = Student(id=id)
+                attendanceEntry = Attendance(eventID=event.id, studentID=id, isNew=1, semester=date.season, year=date.year)
+                db.session.add(newStudent)
+            else:
+                attendanceEntry = Attendance(eventID=event.id, studentID=id, isNew=0, semester=date.season, year=date.year)
+
+            db.session.add(attendanceEntry)
+            db.session.commit()
+
+            if isNew:
+                flash(f'Welcome New Student!', 'success')
+            else:
+                flash(f'Welcome {student.firstName} {student.lastName}!', 'success')
+
+            return redirect(url_for('adminConsole_eventSession', event_url=event.id))
+        else:
+            if student.firstName == 'new_student':
+                flash(f'New Student ({student.id}) has already checked in!', 'danger')
+            else:
+                flash(f'{student.firstName} {student.lastName} has already checked in!', 'danger')
+            return redirect(url_for('adminConsole_eventSession', event_url=event.id))
+
+    return render_template('event_session.html', title='event-session', form=form, event=event)
 
 @app.route("/logout")
 def logout():
