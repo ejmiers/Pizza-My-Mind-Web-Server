e19e266af15f846e1ef9b25ff20be9c85f773965
Eric Miers <ejmiers@msn.com>
2019-04-29 01:34:24 -0400

    Added NewEventForm and IDReaderForm

--- pmm_server/forms.py
+++ pmm_server/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from pmm_server.date_models import Date
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 
 class StudentSignInForm(FlaskForm):
@@ -10,3 +11,13 @@ class AdminSignInForm(FlaskForm):
     adminEmail = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     login = SubmitField('Login')
+
+class NewEventForm(FlaskForm):
+    date = Date()
+    eventName = StringField('Event Name', validators=[DataRequired()])
+    attendance = SelectField('Use Attendance From', validators=[DataRequired()])
+    startNew = SubmitField('Start Event')
+
+
+class IDReaderForm(FlaskForm):
+    id = StringField('Student ID', validators=[DataRequired()])
